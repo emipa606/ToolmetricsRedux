72a9ad62338d0d5d3a75b397b893f51f4487f275
Mlie <emipa606@users.noreply.github.com>
2020-03-19 20:04:36 +0100

    First publish

---
+++ Source/ModCheck/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: AssemblyTitle("ModCheck")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ModCheck")]
+[assembly: AssemblyCopyright("Copyright ©  2017-2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("796a79a2-6754-4847-9910-28de0a4e5ff1")]
+[assembly: AssemblyFileVersion("1.8.0.0")]
---
+++ Source/ModCheck/VersionChecker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ModCheck
+{
+    // Token: 0x02000017 RID: 23
+    internal class VersionChecker
+	{
+		// Token: 0x02000028 RID: 40
+		private class DLLinfo
+		{
+			// Token: 0x0600008A RID: 138 RVA: 0x000044AC File Offset: 0x000026AC
+			public DLLinfo(string newMod, Version newVersion)
+			{
+				this.mod = newMod;
+				this.version = newVersion;
+			}
+
+			// Token: 0x0400005D RID: 93
+			public string mod;
+
+			// Token: 0x0400005E RID: 94
+			public Version version;
+		}
+	}
+}
